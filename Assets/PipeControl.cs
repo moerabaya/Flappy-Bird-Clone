bb18caecfc54dc4ed6d8b17fa1e9eb8a6d60a47e
Mohammed Rabay'a <rabaya.moe@gmail.com>
2023-06-26 23:41:32 +0200

    Add pipe

---
+++ Assets/PipeControl.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeControl : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float deadZone = -10; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePipe();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePipe();
+    }
+
+    void movePipe()
+    {
+        transform.position += (Vector3.left * moveSpeed) * Time.deltaTime;
+        if(transform.position.x <= deadZone)
+        {
+            Destroy(transform);
+        }
+    }
+}
