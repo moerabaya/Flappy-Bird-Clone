e8f7f07fc12b6fa31eea9194cd09f44f5ca268f8
Mohammed Rabay'a <rabaya.moe@gmail.com>
2023-06-27 21:06:42 +0200

    Create LogicManager.cs

---
+++ Assets/LogicManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicManager : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOver;
+
+    [ContextMenu("Increase  Score")]
+    public void addScore(int scoreToAdd)
+    {
+        playerScore += scoreToAdd;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        gameOver.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void showGameOver()
+    {
+        gameOver.SetActive(true); 
+    }
+}
