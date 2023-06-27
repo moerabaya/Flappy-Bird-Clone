a5c8451672d9bbbbf7bf8fb2957260ee2b9f8fa1
Mohammed Rabay'a <rabaya.moe@gmail.com>
2023-06-27 21:06:44 +0200

    Update BirdMove.cs

--- Assets/BirdMove.cs
+++ Assets/BirdMove.cs
@@ -6,10 +6,12 @@ public class BirdMove : MonoBehaviour
 {
     public Rigidbody2D rigidbody2D;
     public int flapStength = 6;
+    public LogicManager logic;
+    public bool birdAlive = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicManager>();
     }
 
     // Update is called once per frame
@@ -20,9 +22,15 @@ public class BirdMove : MonoBehaviour
 
     void moveBirdUp()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && birdAlive)
         {
             rigidbody2D.velocity = Vector2.up * flapStength;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        birdAlive = false;
+        logic.showGameOver();
     }
 }
